b1b7d8682b8e11718cf4694849c1bd6984c154de
faisal gaur <faisal07ali@gmail.com>
2022-07-09 08:39:45 +0530

    update code

--- plot_sensor_data.py
+++ plot_sensor_data.py
@@ -5,8 +5,7 @@ import os
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-path = r'D:\sense_data'
-all_files = glob.glob(os.path.join(path, "*.csv"))
+all_files = glob.glob(os.path.join('sense_data', "*.csv"))
 df = pd.concat((pd.read_csv(f) for f in all_files))
 
 # Create figure
